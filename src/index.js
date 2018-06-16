0c8f0503248854d19335719edc038dc23a4fcd66
vokamura <vcokamura@gmail.com>
2018-06-16 10:54:59 -0700

    completed react-intro

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const user = {
+    name: "Vikki",
+    luckyNumber: luckyNumber()
+    };
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    // <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
+
+function luckyNumber() {
+    return Math.floor(Math.random()*1000);
+}
+
+
+function greeting (User) {    
+    let container = (
+        <div className = "container">
+            <h1>Hello {user.name}</h1>
+            <h2 className = "text-muted">Your lucky number is: {user.luckyNumber}</h2>
+        </div>
+    );
+    return container;
+}
+
+
+
